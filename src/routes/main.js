1e9b0d3cb58736b77795fcfc89263cf05246ec31
scourge <hamzaofthedesert@gmail.com>
2024-07-11 18:06:43 -0400

    Remove node_modules from Git tracking

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
-
+const generateCalendar = require('../services/calendarGenerator');
+// Serve the index.html file
 router.get('/', (req, res) => {
     res.sendFile('index.html', { root: 'public' });
+});
+
+// // New route for generating the calendar
+router.get('/calendar', (req, res) => {
+    const startDate = new Date('2024-07-02');
+    const endDate = new Date('2024-08-22');
+    const calendar = generateCalendar(startDate, endDate);
+    res.json(calendar); 
 });
 
 module.exports = router;
--- src/services/calendarGenerator.js
+++ src/services/calendarGenerator.js
@@ -6,23 +6,29 @@
  * @param {Date} startDate - The start date of the calendar.
  * @param {Date} endDate - The end date of the calendar.
  * @return {Array} An array of objects representing each day in the calendar, 
- *                 with properties 'day' and 'date'.
+ *                 with properties 'day' and 'date'.()
  */
 function generateCalendar(startDate, endDate) {
     const calendar = [];
     let currentDate = new Date(startDate);
     const end = new Date(endDate);
-
+  
     while (currentDate <= end) {
-        //const formattedDate = currentDate.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' });
-        calendar.push({
-            day: calendar.length + 1,
-            date: formattedDate
-        });
-        currentDate.setDate(currentDate.getDate() + 1);
+      const formattedDate = currentDate.toLocaleDateString('en-US', {
+        weekday: 'short',
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric'
+      });
+  
+      calendar.push({
+        day: calendar.length + 1,
+        date: formattedDate
+      });
+      currentDate.setDate(currentDate.getDate() + 1);
     }
-
+  
     return calendar;
-}
+  }
 
 module.exports = generateCalendar;
